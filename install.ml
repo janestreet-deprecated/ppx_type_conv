43a747e39f8d817591ee80f02f941dfee126070a
Jeremie Dimino <jdimino@janestreet.com>
2016-05-26 09:24:26 +0100

    113.44+61

--- install.ml
+++ install.ml
@@ -5,6 +5,6 @@ open Oasis2opam_install;;
 
 generate ~package:"ppx_type_conv"
   [ oasis_lib "ppx_type_conv"
-  ; file "META" ~section:"lib"
   ; oasis_lib "ppx_type_conv_deriving"
+  ; file "META" ~section:"lib"
   ]
