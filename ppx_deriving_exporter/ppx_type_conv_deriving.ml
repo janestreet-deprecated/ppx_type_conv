dd5dee0c068d0472941c43aec51fc8e188c62652
Jeremie Dimino <jdimino@janestreet.com>
2016-09-06 16:26:50 +0100

    Fix #4 and Closed #5
    Make the ppx_deriving glue more resilient to small changes in
    ppx_deriving. Related to whitequark/ppx_deriving#94

--- ppx_deriving_exporter/ppx_type_conv_deriving.ml
+++ ppx_deriving_exporter/ppx_type_conv_deriving.ml
@@ -1,1 +1,25 @@
-Ppx_type_conv.Std.Type_conv.Ppx_deriving_exporter.set (module Ppx_deriving)
+Ppx_type_conv.Std.Type_conv.Ppx_deriving_exporter.set
+  (module struct
+    type deriver = Ppx_deriving.deriver
+
+    let create
+          name
+          ?core_type
+          ?type_ext_str
+          ?type_ext_sig
+          ?type_decl_str
+          ?type_decl_sig
+          ()
+      =
+      Ppx_deriving.create
+        name
+        ?core_type
+        ?type_ext_str
+        ?type_ext_sig
+        ?type_decl_str
+        ?type_decl_sig
+        ()
+    ;;
+
+    let register deriver = Ppx_deriving.register deriver
+  end)
