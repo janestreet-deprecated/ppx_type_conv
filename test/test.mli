12c543b95834cd7c4dca9294fa137bace4f4157d
Thomas Refis <thomas.refis@gmail.com>
2017-12-06 12:56:56 +0000

    v0.9.116.03+50

--- test/test.mli
+++ test/test.mli
@@ -5,3 +5,7 @@ val t_of_sexp : Sexplib.Sexp.t -> t
 val sexp_of_t : t -> Sexplib.Sexp.t
 val compare : t -> t -> int
 [@@@end]
+
+type u = int [@@deriving_inline bin_shape]
+val bin_shape_u : Bin_prot.Shape.t
+[@@@end]
